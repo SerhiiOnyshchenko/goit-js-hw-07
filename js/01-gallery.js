b5852628df6be381c3dc6f151e5e68a68d8bf887
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-04 19:21:01 +0200

    fixing dz1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEBox = document.querySelector('.gallery');
-for (const { preview, original, description } of galleryItems) {
-   galleryEBox.insertAdjacentHTML(
-      'beforeend',
-      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
-   );
-}
-galleryEBox.insertAdjacentHTML(
-   'afterend',
-   `<div class="basicLightbox"><img class="basicLightbox__placeholder" src="" alt=""/></div>`
-);
-const basicLightboxEL = document.querySelector('.basicLightbox');
-const basicLightboxImg = document.querySelector('.basicLightbox__placeholder');
+// create images
+const arrImages = galleryItems.map(({ preview, original, description }) => {
+   return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+});
+galleryEBox.insertAdjacentHTML('beforeend', arrImages.join(''));
+// open modal window
 galleryEBox.addEventListener(
    'click',
    event => {
       if (event.target.closest('.gallery__link')) {
          event.preventDefault();
-         basicLightboxImg.src = event.target.closest('.gallery__link').href;
-         basicLightboxEL.classList.add('basicLightbox--visible');
+         const instance = basicLightbox.create(
+            `<img src="${event.target.closest('img').dataset.source}" width="800" height="600">`
+         );
+         instance.show();
       }
    },
    false
 );
-basicLightboxEL.addEventListener('click', () => {
-   basicLightboxEL.classList.remove('basicLightbox--visible');
-});
+// close modal window key Escape
 document.addEventListener('keyup', event => {
-   if ((event.code === 'Escape') & (basicLightboxEL.classList[1] === 'basicLightbox--visible')) {
-      basicLightboxEL.classList.remove('basicLightbox--visible');
+   const basicLightboxEL = document.querySelector('.basicLightbox');
+   if ((event.code === 'Escape') & (basicLightboxEL !== null)) {
+      basicLightboxEL.remove();
    }
 });
