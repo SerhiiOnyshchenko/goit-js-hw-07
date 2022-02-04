800461e403e8f1572ddf6fbc3e21d1bbf90e138b
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-04 22:03:28 +0200

    dz 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEBox = document.querySelector('.gallery');
+// create images
+const arrImages = galleryItems.map(({ preview, original, description }) => {
+   return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a>`;
+});
+galleryEBox.insertAdjacentHTML('beforeend', arrImages.join(''));
+// open modal window
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+   gallery.defaultOptions.captionDelay = 250;
+});
 
-console.log(galleryItems);
+gallery.on('error.simplelightbox', function (e) {
+   console.log(e); // some usefull information
+});
